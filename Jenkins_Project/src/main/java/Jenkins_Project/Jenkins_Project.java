4310a300c67a8b7ac92a480d3c5347c328f74af2
Anil Chaudhary <44877418+anilchaudhary001@users.noreply.github.com>
2026-04-25 12:36:30 +0000

    add jenkinsfile

--- Jenkins_Project/src/main/java/Jenkins_Project/Jenkins_Project.java
+++ Jenkins_Project/src/main/java/Jenkins_Project/Jenkins_Project.java
@@ -6,6 +6,8 @@ public class Jenkins_Project {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Jenkins");
+		Library lib = new Library();
+		System.out.println("Library method result: " + lib.someLibraryMethod());
 	}
 
 }
